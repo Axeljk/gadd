2c528a9217a228ffcd385e9d333cddcab4f61b40
Axel <axeljkern@yahoo.com>
2022-06-09 23:26:30 -0400

    Alphabetizes constants and organizes them into catagories.

--- assets/src/constants.js
+++ assets/src/constants.js
@@ -1,3 +1,5 @@
-export const kTileSize = 16;
-export const kMinTilesVisible = 10;
+// Constants regarding the map.
 export const kMaxTilesVisible = 16;
+export const kMinTilesVisible = 10;
+export const kPremadeMaps = ["lvl1", "lvl2"];
+export const kTileSize = 16;
